b0ed85c240853a0c45f153b63a4a9e6d82c0bd8a
devsooki <devsooki@gmail.com>
2024-03-25 16:55:06 +0900

    feat: init setting

---
+++ src/style/globalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    font-family: 'Nanum Gothic', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  ul,li {
+    list-style: none;
+  }
+  input, textarea {
+    outline: none;
+  }
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+`;
