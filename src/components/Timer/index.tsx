662e7eb4ba2fae9457017579086bf858ecb81d0f
devsooki <devsooki@gmail.com>
2024-04-02 21:27:17 +0900

    feat: 버튼으로 타이머 작동, 0초 되면 초기 값으로 리셋

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,25 +3,40 @@ import styled from 'styled-components'
 
 const Timer = () => {
   // TODO: 초기 기본 설정 타이머 값 3분, 이후 시간 설정 기능 만든 후 변경 예정
-  const initialTime = 180;
-  const [remainingTime, setRemainingTime] = useState<number>(initialTime);
+  const [time, setTime] = useState<number>(180)
+  const [remainingTime, setRemainingTime] = useState<number>(time);
+  const [timerOn, setTimerOn] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (time === 0) {
+      // TODO: 시간 설정하도록 해야함
+    }
+  }, [])
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (remainingTime > 0) {
+      if (timerOn && remainingTime > 0) {
         setRemainingTime(prev => prev - 1);
-      } else {
+      } else if (remainingTime < 0) {
         // TODO: 10초 미만으로 남을 경우 강조 색상으로 변경
         clearInterval(timer)
+      } else if (remainingTime === 0) {
+        setRemainingTime(time)
+        setTimerOn(false)
       }
     }, 1000);
 
     return () => clearInterval(timer)
-  }, [remainingTime])
+  }, [remainingTime, timerOn])
+
+  const onChangeTimerOn = () => {
+    setTimerOn(!timerOn)
+  }
 
   // TODO: 초기 값 변경되서 h도 생기면 포맷 변경 필요
   const minute = String(Math.floor(remainingTime / 60)).padStart(2, '0');
   const second = String(Math.floor(remainingTime % 60)).padStart(2, '0');
+
 
   return (
     <Container>
@@ -29,6 +44,10 @@ const Timer = () => {
         <span>타이머</span>
         <b>{minute} : {second}</b>
       </Content>
+
+      <TimerButton onClick={onChangeTimerOn}>
+        <div className={timerOn ? 'on' : 'off'}/>
+      </TimerButton>
     </Container>
   )
 }
@@ -58,4 +77,29 @@ const Content = styled.div`
     font-size: 50px;
     letter-spacing: -1px;
   }
+`
+const TimerButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 1px solid #ff4040;
+  background-color: #fff;
+
+  .on {
+    width: 15px;
+    height: 15px;
+    background-color: #ff4040;
+  }
+  .off {
+    width: 0;
+    height: 0;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    border-left: 15px solid #ff4040;  
+  }
+
 `
